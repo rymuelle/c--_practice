a71ac3e6d28599e273440ebc9312a1930af05353
rymuelle <rmueller@physics.tamu.edu>
2014-05-16 15:37:30 -0500

    sorting program homework

---
+++ sort.cpp
@@ -0,0 +1,17 @@
+/* hopefully this is a sorting alogrythm*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+	int array_length;
+	
+	cout << "What is the length?" << endl;
+	std::cin >> array_length;
+	std::cout << array_length;
+
+
+
+}
