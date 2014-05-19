dfffe23753c53a9beab6734d6ff18bf9ddb338cf
rymuelle <rmueller@physics.tamu.edu>
2014-05-19 16:10:41 -0500

    some more progress on sort.cpp, added an array and a loop! next the actual sorting alg.

--- sort.cpp
+++ sort.cpp
@@ -7,11 +7,24 @@ int main()
 {
 
 	int array_length;
-	
+	int sort_array [100];		
 	cout << "What is the length?" << endl;
 	std::cin >> array_length;
-	std::cout << array_length;
-
-
-
+	//std::cout << array_length;
+	if (array_length <100 && array_length > 0) //makes sure the length of the list can fit in array
+	{
+		for (int i = array_length; i>0 ; i--)
+		{
+			cout <<"Imput" << i << endl;
+			cin >> sort_array[i];
+		//	cout << sort_array[i];
+		}
+		
+		
+	} else if (array_length >= 100 || array_length <= 0)	//catching exceptions
+	{
+		cout << "Nope!";
+	}
+	
 }
+
